d0bc243f152b41734b53800b82655fee5ed7c0c5
Benni <ben.athiwaratkun@gmail.com>
2014-05-22 17:14:01 -0400

    second commit

---
+++ training/t.py
@@ -0,0 +1,11 @@
+from numpy import *
+import scipy as sp
+from sklearn import svm
+from sklearn import cross_validation as cv
+from sklearn import linear_model as lm
+import pybrain as pb
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from scipy import stats
+import pickle
+
